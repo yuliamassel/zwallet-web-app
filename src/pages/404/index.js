c081f441e4933e48528210f987e41de0afdc3b1f
ferdif <ferdifh82@gmail.com>
2022-01-11 07:51:14 +0700

    modify page not found

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,10 +1,17 @@
 import React, { Fragment } from "react";
+import img from "../../assets/img/404.jpg";
+import "./404.css";
 
 const Page404 = () => {
   return (
     <Fragment>
-      <h1 className="text-center">404</h1>
-      <h3 className="text-center">Page Not Found</h3>
+      <div className="page-not-found-wrapper d-flex flex-column justify-content-center align-items-center">
+        <div className="page-not-found-text mt-3">
+          <h1 className="text-center">404</h1>
+          <h3 className="text-center">Page Not Found</h3>
+        </div>
+        <img src={img} className="img-not-found" alt="404" width="800px" />
+      </div>
     </Fragment>
   );
 };
