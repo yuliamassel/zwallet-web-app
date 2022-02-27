97acb77ebaa117a202b9a53d6978fe0f731ac3c6
ferdif <ferdifh82@gmail.com>
2022-02-27 14:31:16 +0700

    integrating income and expense data in dashboard page

--- src/components/module/Charts/index.js
+++ src/components/module/Charts/index.js
@@ -1,10 +1,14 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import * as BsIcons from "react-icons/bs";
 import * as FiIcons from "react-icons/fi";
+import { UserContext } from "../../../context/UserContext";
 import "./chart.css";
 // import graphic from "../../../assets/img/graphic.png";
 
 const Chart = () => {
+  // eslint-disable-next-line no-unused-vars
+  const { user, setUser } = useContext(UserContext);
+  console.log(user);
   return (
     <Fragment>
       <section className="col-lg-6 history-content-chart d-flex flex-column ms-3">
@@ -12,12 +16,12 @@ const Chart = () => {
           <div className="income">
             <BsIcons.BsArrowDown className="icons-size text-green" />
             <p className="financial-text">Income</p>
-            <p className="financial-cash">Rp 0</p>
+            <p className="financial-cash">Rp {user.income}</p>
           </div>
           <div className="expense">
             <BsIcons.BsArrowUp className="icons-size text-red" />
             <p className="financial-text">Expense</p>
-            <p className="financial-cash">Rp 0</p>
+            <p className="financial-cash">Rp {user.expense}</p>
           </div>
         </div>
         <div className="graphic d-flex flex-column justify-content-center align-items-center">
