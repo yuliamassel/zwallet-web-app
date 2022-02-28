9d9862b4d499c147a0c4c9f99a902f19507eec04
ferdif <ferdifh82@gmail.com>
2022-02-28 09:23:39 +0700

    edit redux store path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/store/store";
 
 ReactDOM.render(
   <React.StrictMode>
